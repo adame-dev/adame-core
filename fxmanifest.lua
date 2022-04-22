99c2f4e8fdeead7dad01d8d7a57aee9de66c2ade
Javi Adame <35896509+NickBlade@users.noreply.github.com>
2022-04-22 14:20:17 +0200

    Upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant' 
+game 'gta5' 
+description 'FiveM Framework used for Adame projects.' 
+version '1.0.0' 
+server_scripts { 
+	'config.lua', 
+	'server/main.lua' 
+} 
+client_scripts { 
+	'config.lua', 
+	'client/main.lua' 
+} 
